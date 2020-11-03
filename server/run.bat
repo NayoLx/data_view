e2051589542c1ea72c0995b7964270df7a42fa17
coco <690575679@qq.com>
2020-11-03 15:42:34 +0800

    U 4.bat

--- server/run.bat
+++ server/run.bat
@@ -1,5 +1,11 @@
+@Rem 运行build好的项目，然后打开浏览器
 @echo off
 
-node app.js
+start node app.js
+
+@Rem 修改浏览器位置
+cd C:\Program Files (x86)\Google\Chrome\Application
+
+start chrome.exe http://localhost:3000
 
 echo start node app.js
